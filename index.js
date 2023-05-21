b8ff59d100c4f7b332a0bd00f558c8c85d34a760
minjucse <minjucse@gmail.com>
2023-05-21 10:50:30 +0600

    vercel added

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ require('dotenv').config()
 // middleware
 app.use(cors());
 app.use(express.json());
+console.log(process.env.DB_USER)
 
-const uri =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.goerh3z.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.goerh3z.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,8 +26,32 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
+
+    const productsCollection = client.db('toyMertDB').collection('products');
+    app.get('/', (req, res) => {
+      res.send('Server Api is running')
+    });
+
+    app.post("/post-product", async (req, res) => {
+      const body = req.body;
+      body.createdAt = new Date();
+      console.log(body);
+      const result = await productsCollection.insertOne(body);
+      if (result?.insertedId) {
+        return res.status(200).send(result);
+      } else {
+        return res.status(404).send({
+          message: "can not insert try again leter",
+          status: false,
+        });
+      }
+    });
+
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -34,10 +59,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Server Api is running')
-})
+
 
 app.listen(port, () => {
-    console.log(`Server API is running on port ${port}`)
+  console.log(`Server API is running on port ${port}`)
 })
